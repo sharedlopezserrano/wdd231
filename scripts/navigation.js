1328de2359826b7ea445a6bc54190152ff2697ac
SharedLopez <shared@socrates.la>
2025-08-06 03:42:43 -0300

    Week 04 homework

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -5,3 +5,15 @@ navbutton.addEventListener("click", () => {
     navbutton.classList.toggle("show");
     navBar.classList.toggle("show");
 });
+
+const openButton = document.querySelector("#btn-all");
+const dialogBox = document.querySelector("#dialogBox");
+const closeButton = document.querySelector("#closeButton");
+
+openButton.addEventListener("click", () => {
+    dialogBox.showModal();
+});
+
+closeButton.addEventListener("click", () => {
+  dialogBox.close();
+});
